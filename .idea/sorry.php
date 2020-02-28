0b178275257c14898fff7de55499e59d815bf3ee
imen5581 <Chernyahovsky2010@yandex.ru>
2020-02-28 16:05:29 +0300

    Create sorry.php

---
+++ .idea/sorry.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Вы ввели неправильные данные</title>
+    <link rel="stylesheet" href="sorry.css">
+</head>
+<body>
+<div class="sorry">Введите пожалуйста корректные данные
+<img src="https://stopga.me/images/news/2016/06/14/S9uo8kA1.jpg" class="krash"/>
+    <form action="index.php">
+        <button class="btn" type="submit">Ввести заново</button>
+    </form>
+</div>
+
+</body>
+</html>
