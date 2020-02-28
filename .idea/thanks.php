8c9ca8eabfe213d7988293acb03dd57771e6bb26
imen5581 <Chernyahovsky2010@yandex.ru>
2020-02-28 16:06:22 +0300

    Create thanks.php

---
+++ .idea/thanks.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Спасибо</title>
+</head>
+<body>
+
+<div class="thanks">Спасибо, информация отправлена на Ваш Email адресс!</div>
+
+</body>
+</html>
